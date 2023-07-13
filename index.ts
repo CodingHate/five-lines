f2e0894d14754ddfe70bb0b5cf6331f559872054
hyunwoo <junghw86@quantamatrix.com>
2023-07-14 08:48:35 +0900

    study: ex 5.65 조건 문 줄이기

--- index.ts
+++ index.ts
@@ -844,10 +844,7 @@ function updateMap() {
 }
 
 function updateTile(x: number, y: number) {
-  if (
-    (map[y][x].isStone() && map[y + 1][x].isAir()) ||
-    (map[y][x].isBox() && map[y + 1][x].isAir())
-  ) {
+  if ((map[y][x].isStone() || map[y][x].isBox()) && map[y + 1][x].isAir()) {
     map[y][x].drop();
     map[y + 1][x] = map[y][x];
     map[y][x] = new Air();
